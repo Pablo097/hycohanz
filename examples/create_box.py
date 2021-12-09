8e640d9d4e95ae6d3199beefc43296b2acf0081e
Mateos Ruiz <pmateosr@ic.uma.es>
2021-12-09 18:54:21 +0100

    fixed tabs and spaces

--- examples/create_box.py
+++ examples/create_box.py
@@ -1,19 +1,19 @@
 import hycohanz as hfss
 
-raw_input('Press "Enter" to connect to HFSS.>')
+input('Press "Enter" to connect to HFSS.>')
 
 with hfss.App() as App:
 
-    raw_input('Press "Enter" to create a new project.>')
+    input('Press "Enter" to create a new project.>')
     with hfss.NewProject(App.oDesktop) as P:
 
-        raw_input('Press "Enter" to insert a new DrivenModal design named HFSSDesign1.>')
+        input('Press "Enter" to insert a new DrivenModal design named HFSSDesign1.>')
         with hfss.InsertDesign(P.oProject, "HFSSDesign1", "DrivenModal") as D:
 
-            raw_input('Press "Enter" to set the active editor to "3D Modeler" (The default and only known correct value).>')
+            input('Press "Enter" to set the active editor to "3D Modeler" (The default and only known correct value).>')
             with hfss.SetActiveEditor(D.oDesign) as E:
 
-                raw_input('Press "Enter" to draw a red box named Box1.>')
+                input('Press "Enter" to draw a red box named Box1.>')
 
                 hfss.create_box(E.oEditor,  
                                 1, 
@@ -21,7 +21,8 @@ with hfss.App() as App:
                                 3, 
                                 4,
                                 5,
+                                6,
                                 Name='Box1',
                                 Color=(255, 0, 0))
 
-                raw_input('Press "Enter" to release HFSS.>')
+                input('Press "Enter" to release HFSS.>')
--- examples/uncover_faces.py
+++ examples/uncover_faces.py
@@ -1,22 +1,22 @@
 import hycohanz as hfss
 
-raw_input('Press "Enter" to connect to HFSS.>')
+input('Press "Enter" to connect to HFSS.>')
 
 [oAnsoftApp, oDesktop] = hfss.setup_interface()
 
-raw_input('Press "Enter" to create a new project.>')
+input('Press "Enter" to create a new project.>')
 
 oProject = hfss.new_project(oDesktop)
 
-raw_input('Press "Enter" to insert a new DrivenModal design named HFSSDesign1.>')
+input('Press "Enter" to insert a new DrivenModal design named HFSSDesign1.>')
 
 oDesign = hfss.insert_design(oProject, "HFSSDesign1", "DrivenModal")
 
-raw_input('Press "Enter" to set the active editor to "3D Modeler" (The default and only known correct value).>')
+input('Press "Enter" to set the active editor to "3D Modeler" (The default and only known correct value).>')
 
 oEditor = hfss.set_active_editor(oDesign)
 
-raw_input('Press "Enter" to draw a red three-vertex polyline named Triangle1.>')
+input('Press "Enter" to draw a red three-vertex polyline named Triangle1.>')
 
 objname = hfss.create_polyline(
     oEditor, [1, 0, -1], 
@@ -26,17 +26,17 @@ objname = hfss.create_polyline(
     Color="(255 0 0)",
     )
 
-raw_input('Press "Enter" to get a handle to the polyline face.>')
+input('Press "Enter" to get a handle to the polyline face.>')
 
 faceid = hfss.get_face_by_position(oEditor, objname, 0, 0.5, 0)
 
 print('faceid: ' + str(faceid))
 
-raw_input('Press "Enter" to uncover the polyline face.>')
+input('Press "Enter" to uncover the polyline face.>')
 
 hfss.uncover_faces(oEditor, [objname], {objname: [faceid]})
 
-raw_input('Press "Enter" to quit HFSS.>')
+input('Press "Enter" to quit HFSS.>')
 
 hfss.quit_application(oDesktop)
 
